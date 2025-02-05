443bbb8dc0d0c7a38763a9cb4ababea43419bc41
Ssaava <ssavaemma4@gmail.com>
2025-02-05 20:08:02 +0300

    finalized with the UI

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   imports: [],
   templateUrl: './card.component.html',
-  styleUrl: './card.component.css'
+  styleUrl: './card.component.css',
 })
 export class CardComponent {
-
+  @Input() backgroundColor: string = 'bg-purple-400';
+  @Input() title: string = 'Counts';
+  @Input() totalCount: string = '';
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { FormComponent } from '../../components/form/form.component';
+import { CardComponent } from '../../components/card/card.component';
+import { FooterComponent } from '../../components/footer/footer.component';
 
 @Component({
   selector: 'app-home',
-  imports: [FormComponent, FormComponent],
+  imports: [FormComponent, FormComponent, CardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
